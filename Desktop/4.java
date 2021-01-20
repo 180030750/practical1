db4bd59842ae35e6985e43f82a62e2b81685dd8f
180030750 <76678192+180030750@users.noreply.github.com>
2021-01-20 16:30:56 +0530

    Update 4.java

--- Desktop/4.java
+++ Desktop/4.java
@@ -1,1 +1,16 @@
-This is a sample java file
+import java.util.Scanner;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+       
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+
+       
+        int number = reader.nextInt();
+
+        System.out.println("You entered: " + number);
+    }
+}
