18dd821b2fce6f8bbda048cf60791dfa4546b3ad
180030750 <76678192+180030750@users.noreply.github.com>
2021-01-20 16:32:42 +0530

    Update 2.c

--- Desktop/2.c
+++ Desktop/2.c
@@ -1,1 +1,13 @@
-This is sample c file
+#include <stdio.h>
+int main() {   
+    int number;
+   
+    printf("Enter an integer: ");  
+    
+    scanf("%d", &number);
+
+
+    printf("You entered: %d", number);
+    
+    return 0;
+}
